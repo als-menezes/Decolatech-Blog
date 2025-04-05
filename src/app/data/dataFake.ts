a8e4a20b865e8b58175fe07e5946a24f387d2fb7
als-menezes <andre.sousamenezes2@gmail.com>
2025-04-05 17:50:40 -0300

    > All-in-one

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Notas do patch de 1º de abril",
+    "description": "Se tem uma coisa que sabemos sobre os jogadores de WoW é que vocês querem mais. E, neste patch, vamos ter mais de tudo.",
+    "photoCover":"https://bnetcmsus-a.akamaihd.net/cms/blog_thumbnail/q6/Q6I9FYQQLYST1743115791962.png"
+  },
+  {
+    "id":"2",
+    "title": "Como participar do evento de 30 anos de Warcraft?",
+    "description": "Último lote de ingressos gratuitos estarão disponíveis no site da Tixr a partir de 7 de abril; evento acontece no dia 19 de abril.",
+    "photoCover":"https://bnetcmsus-a.akamaihd.net/cms/blog_header/gy/GYPHU7WH175X1655253137144.png"
+  }
+]
